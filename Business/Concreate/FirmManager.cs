cde608d12b5db96283ee9acbbea8822611316820
kevsernury <68063554+kevsernury@users.noreply.github.com>
2021-06-25 03:03:06 +0300

    Business

---
+++ Business/Concreate/FirmManager.cs
@@ -0,0 +1,18 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concreate
+{
+    public class FirmManager : IFirmService
+    {
+        IFirmDal _firmDal;
+
+        public FirmManager(IFirmDal firmDal)
+        {
+            _firmDal = firmDal;
+        }
+    }
+}
---
+++ Business/Concreate/FlightManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concreate
+{
+    public class FlightManager : IFlightService
+    {
+        IFlightDal _flightDal;
+
+        public FlightManager(IFlightDal flightDal)
+        {
+            _flightDal = flightDal;
+        }
+
+        public void Add(Flight flight)
+        {
+            _flightDal.Add(flight);
+
+        }
+
+        public List<Flight> GetAll()
+        {
+            return _flightDal.GetAll();
+
+        }
+
+        public List<Flight> GetAllByBusinessPrice(decimal min, decimal max)
+        {
+            return _flightDal.GetAll(f => f.BusinessPrice >= min && f.BusinessPrice <= max);
+        }
+
+        public List<Flight> GetAllByEcoPrice(decimal min, decimal max)
+        {
+            return _flightDal.GetAll(f => f.EcoPrice >= min && f.EcoPrice <= max);
+        }
+
+        public List<Flight> GetAllByFirmId(int id)
+        {
+            return _flightDal.GetAll(f => f.FirmID == id);
+        }
+
+        public List<Flight> GetAllByFlightId(int id)
+        {
+            return _flightDal.GetAll(f => f.ID == id);
+        }
+    }
+}
