4dcd5b382aecddb1d54945918807adf9ed34a764
kevsernury <68063554+kevsernury@users.noreply.github.com>
2021-06-25 03:22:06 +0300

    Add files via upload

---
+++ ReservationForm/Program.cs
@@ -0,0 +1,24 @@
+using ReservationForm.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReservationForm
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
